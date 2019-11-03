31acc399ab2577749930ce91f3fdc00b06c62c1a
Anton Kosar <kosarantoni@gmail.com>
2019-11-03 22:45:53 +0100

    add apple generator add direction check add snake grow after eating

--- script.js
+++ script.js
@@ -1,93 +1,56 @@
-let position = "0_0";
-let snakeStep;
+let snake = [];
+let crntDirection = "right";
 let fieldSize = 20;
 
+function getRandom(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min)) + min;
+  }
 
-function createField() {
+function addApple() {
+    let appleField = document.getElementById(`${getRandom(0, fieldSize)}_${getRandom(0, fieldSize)}`);
+    appleField.classList.add("apple");
+    return appleField;
+}
+
+function eatApple(pos) {
+    console.log(pos);
+    if (pos.classList.contains("apple")) {
+        pos.classList.remove("apple");
+        addApple();
+        return "nomnom"
+    }
+}
+
+function createField(fieldSize) {
     let gameField = document.querySelector(".game__field");
     
     for (let y = 0; y < fieldSize; y++) {
 
-        let tmpIdFirst = y + "_";
+        let tmpId = y + "_";
 
         for (let x = 0; x < fieldSize; x++) {
 
-            let tmpId = tmpIdFirst + x;
+            let id = tmpId + x;
 
             let fieldBox = document.createElement("div");
             fieldBox.classList.add("square")
-            fieldBox.setAttribute("id", tmpId);
+            fieldBox.setAttribute("id", id);
 
             gameField.append(fieldBox);
         }
     }
 }
 
-createField();
-
-let snake = [
-    document.getElementById("3_3"),
-    document.getElementById("3_2"),
-    document.getElementById("3_1")
-];
-
-// function snakeMove(snakeDirection) {
-//     if (snakeStep) clearTimeout(snakeStep);
-//     let stepX = position.slice(-1);
-//     let stepY = position.slice(0,1);
-
-//     snakeStep = setTimeout(function move() {
-//             let direction = stepY + "_" + stepX;
-
-//             switch(snakeDirection) {
-//                 case "left" : {
-//                     stepX--;
-//                     break;
-//                 };
-//                 case "right" : {
-//                     stepX++;
-//                     break;
-//                 };
-//                 case "up" : {
-//                     stepY--;
-//                     break;
-//                 };
-//                 case "down" : {
-//                     stepY++;
-//                     break;
-//                 };
-//             };
-
-//             if (stepX > 7) {
-//                 stepX = 0; 
-//             };
-//             if (stepX < 0) {
-//                 stepX = 7; 
-//             };
-//             if (stepY > 7) {
-//                 stepY = 0; 
-//             };
-//             if (stepY < 0) {
-//                 stepY = 7; 
-//             };
-
-//             removeSquare(position);
-//             position = direction;
-//             addSquare(direction);
-//             snakeStep = setTimeout(move, 500);
-//         });
-// }
-// function addSquare(id) {
-//     let tmpBox = document.getElementById(id);
-//     tmpBox.classList.add("snake");
-// }
-
-// function removeSquare(id) {
-//     let tmpBox = document.getElementById(id);
-//     tmpBox.classList.remove("snake");
-// }
-
-function createSnake(snake) {
+function createSnake() {
+    if (snake.length == 0) {
+        snake = [
+            document.getElementById("3_3"),
+            document.getElementById("3_2"),
+            document.getElementById("3_1")
+        ];
+    }
     for (let snakePart of snake) {
         snakePart.classList.add("snake");
     }
@@ -103,42 +66,69 @@ function posCheck(step) {
     }
 }
 
+function checkDirection(direction) {
+    if (direction == "up" && crntDirection == "down") {
+        return crntDirection;
+    }
+    if (direction == "down" && crntDirection == "up") {
+        return crntDirection;
+    }
+    if (direction == "left" && crntDirection == "right") {
+        return crntDirection;
+    }
+    if (direction == "right" && crntDirection == "left") {
+        return crntDirection;
+    }
+    return direction;
+}
 
 function snakeMove(direction) {
     let crntHeadPos = snake[0].id.split("_");
     let posY = crntHeadPos[0];
     let posX = crntHeadPos[1];
+    let snakeStep;
+
+    direction = checkDirection(direction);
 
     switch(direction) {
         case "up" : {
-            snake.unshift( document.getElementById(`${posCheck(--posY)}_${posX}`) );
+            crntDirection = "up";
+            snakeStep = document.getElementById(`${posCheck(--posY)}_${posX}`);
             break;
         }
         case "down" : {
-            snake.unshift( document.getElementById(`${posCheck(++posY)}_${posX}`) );
+            crntDirection = "down";
+            snakeStep = document.getElementById(`${posCheck(++posY)}_${posX}`);
             break;
         }
         case "left" : {
-            snake.unshift( document.getElementById(`${posY}_${posCheck(--posX)}`) );
+            crntDirection = "left";
+            snakeStep = document.getElementById(`${posY}_${posCheck(--posX)}`);
             break;
         }
         case "right" : {
-            snake.unshift( document.getElementById(`${posY}_${posCheck(++posX)}`) );
+            crntDirection = "right";
+            snakeStep = document.getElementById(`${posY}_${posCheck(++posX)}`);
             break;
         }
     }
 
-    snake[snake.length - 1].classList.remove("snake");
-    snake.pop();
+    snake.unshift(snakeStep);
+    if (eatApple(snakeStep) != "nomnom" ) {
+        snake[snake.length - 1].classList.remove("snake");
+        snake.pop();
+    }
+
+    
 
     createSnake(snake)
 }
 
-
+createField(fieldSize);
+createSnake(snake)
+addApple()
 
 document.getElementById("btnUp").addEventListener("click", () => snakeMove("up") );
 document.getElementById("btnDown").addEventListener("click", () => snakeMove("down") );
 document.getElementById("btnLeft").addEventListener("click", () => snakeMove("left") );
 document.getElementById("btnRight").addEventListener("click", () => snakeMove("right") );
-
-createSnake(snake)
