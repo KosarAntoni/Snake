22a9cfb9b40dbec6a0ef9f99e6a5f553bea8e82e
Anton Kosar <kosarantoni@gmail.com>
2019-11-08 19:23:15 +0100

    another try to add gestures XD

--- script.js
+++ script.js
@@ -36,40 +36,82 @@ document.addEventListener("keydown", function(event) {
     }
 })
 
-function getTime() {
-    return new Date().getTime();
-};
+// function getTime() {
+//     return new Date().getTime();
+// };
 
-    document.addEventListener('touchstart', function(e) {
-        xTouch = parseInt(e.touches[0].clientX);
-        yTouch = parseInt(e.touches[0].clientY);
-        stTime = getTime()
-    }, false);
-    document.addEventListener('touchmove', function(e) {
-        e.preventDefault();
-        if(!xTouch || !yTouch) return;
-        xDiff = xTouch - parseInt(e.touches[0].clientX);
-        yDiff = yTouch - parseInt(e.touches[0].clientY);
-        mvTime = getTime();
-        if(Math.abs(xDiff) > 15 && Math.abs(xDiff) > Math.abs(yDiff) && mvTime - stTime < 75) {
-            stTime = 0;
+//     document.addEventListener('touchstart', function(e) {
+//         xTouch = parseInt(e.touches[0].clientX);
+//         yTouch = parseInt(e.touches[0].clientY);
+//         stTime = getTime()
+//         e.preventDefault();
+//     }, false);
+//     document.addEventListener('touchmove', function(e) {
+//         if(!xTouch || !yTouch) return;
+//         xDiff = xTouch - parseInt(e.touches[0].clientX);
+//         yDiff = yTouch - parseInt(e.touches[0].clientY);
+//         mvTime = getTime();
+//         if(Math.abs(xDiff) > 15 && Math.abs(xDiff) > Math.abs(yDiff) && mvTime - stTime < 75) {
+//             stTime = 0;
+//             if(xDiff > 0 && crntDirection != "right") {
+//                 tmpDirection = "left";
+//             }
+//             else if(xDiff < 0 && crntDirection != "left") {
+//                 tmpDirection = "right";
+//             }
+//         }
+//         if(Math.abs(yDiff) > 15 && Math.abs(yDiff) > Math.abs(xDiff) && mvTime - stTime < 75) {
+//             stTime = 0;
+//             if(yDiff < 0 && crntDirection != "up") {
+//                 tmpDirection = "down";
+//             }
+//             else if(yDiff > 0 && crntDirection != "down") {
+//                 tmpDirection = "up";
+//             }
+//         }
+//     }, false)
+
+document.addEventListener('touchstart', handleTouchStart, false);        
+document.addEventListener('touchmove', handleTouchMove, false);
+var xDown = null;                                                        
+var yDown = null;                                                        
+function handleTouchStart(evt) {                                         
+    xDown = evt.touches[0].clientX;                                      
+    yDown = evt.touches[0].clientY;                                      
+};   
+
+function handleTouchMove(evt) {
+    if ( ! xDown || ! yDown ) {
+        return;
+    }
+
+    var xUp = evt.touches[0].clientX;                                    
+    var yUp = evt.touches[0].clientY;
+
+    var xDiff = xDown - xUp;
+    var yDiff = yDown - yUp;
+    if(Math.abs( xDiff )+Math.abs( yDiff )>150){ //to deal with to short swipes
+
+    if ( Math.abs( xDiff ) > Math.abs( yDiff ) ) {/*most significant*/
             if(xDiff > 0 && crntDirection != "right") {
                 tmpDirection = "left";
             }
             else if(xDiff < 0 && crntDirection != "left") {
                 tmpDirection = "right";
-            }
-        }
-        if(Math.abs(yDiff) > 15 && Math.abs(yDiff) > Math.abs(xDiff) && mvTime - stTime < 75) {
-            stTime = 0;
+            }                      
+    } else {
             if(yDiff < 0 && crntDirection != "up") {
                 tmpDirection = "down";
             }
             else if(yDiff > 0 && crntDirection != "down") {
                 tmpDirection = "up";
-            }
-        }
-    }, false)
+            }                                                               
+    }
+    /* reset values */
+    xDown = null;
+    yDown = null;
+    }
+};
 
 document.getElementById("btnStart").addEventListener("click", function() {
     tmpDirection = "right";
